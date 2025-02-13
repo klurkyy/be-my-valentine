1d529b4e27982927a4107f3401af053beec3f432
klurkyy <159873004+klurkyy@users.noreply.github.com>
2025-02-13 15:19:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yeyyy wuvyuuuu";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "anong no?",
+    "ngii",
+    "ihhhhh :(",
+    "sige",
+    "ihhh bibiii kasiiii",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
